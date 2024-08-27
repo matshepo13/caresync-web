f0b81b73575b0bb8c07b9c53971e3fccae6247d1
MatshepoFrak <matshepo.soto@fraktionaldev.com>
2024-08-27 08:21:10 +0200

    removed api

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,17 +2,11 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDHnyILPuE4844-tDYBbtAjn4l7tvPXXek",
-  authDomain: "caresync-a013a.firebaseapp.com",
-  projectId: "caresync-a013a",
-  storageBucket: "caresync-a013a.appspot.com",
-  messagingSenderId: "948454772859",
-  appId: "1:948454772859:web:58f46b1a3833ca05b512de"
+  
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -3,12 +3,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js";
 
 const firebaseConfig ={
-  apiKey: "AIzaSyDHnyILPuE4844-tDYBbtAjn4l7tvPXXek",
-  authDomain: "caresync-a013a.firebaseapp.com",
-  projectId: "caresync-a013a",
-  storageBucket: "caresync-a013a.appspot.com",
-  messagingSenderId: "948454772859",
-  appId: "1:948454772859:web:58f46b1a3833ca05b512de"
+  
 };
 
 
